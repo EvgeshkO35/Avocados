b4da5145df8d83efd85f4885fbcea9af7e75c993
EvgeshkO35 <56804253+EvgeshkO35@users.noreply.github.com>
2021-04-23 13:23:53 -0400

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Evgenii Lysenko on 4/23/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
